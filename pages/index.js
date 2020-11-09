2c08eef6bdfb82eb573242efb33b5c324cbb9429
Johnny Wu <64444867+johnnywu-namebase@users.noreply.github.com>
2020-11-08 23:20:50 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <main className={styles.main}>
 
         <h1 className={styles.title}>
-         <a href="https://www.handshake.org/"><img src="/favicon.png" alt="Handshake Logo" height="80"/>
+         <a href="https://www.handshake.org/"><img src="/favicon.png" alt="Handshake Logo" height="50"/>
       </a> Name Contest
         </h1>
 
@@ -36,7 +36,7 @@ export default function Home() {
     <footer>
       <a
         href="https://www.namebase.io/">
-        <img src="/Namer Community Logo.gif" alt="Namebase Logo" width="75" height="75"/>
+        <img src="/Namer Community Logo.gif" alt="Namebase Logo" width="40" height="40"/>
       </a>
     </footer>
     
